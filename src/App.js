8cc7c47460cdb637eb501b8d76243a038bb2796d
aznaqCre18 <azizaqibs@gmail.com>
2020-04-05 23:11:11 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Landing from './container/landingPage/landingPage'
 
-function App() {
-  return (
-    <>
-      <div className="container flex justify-center mt-20">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img className="w-full" src={require('./profile.jpg')} alt="Display" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-purple-500 text-xl mb-2">
-              Tanjiro Kamado
-            </div>
-            <p className="text-gray-700 text-base">
-              When i’m not coding i switch to netflix with biscuits and cold tea as my companion. <span></span>😜
-            </p>
-          </div>
-          <div className="px-6 py-4">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Software Engineer</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Public Speaker</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Writter</span>
-          </div>
-        </div>
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Landing />
       </div>
-    </>
-  );
+    )
+  }
 }
-
-export default App;
---
+++ src/container/landingPage/landingPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './landingPage.css'
+
+class landingPage extends Component {
+    render() {
+        return (
+            <>
+                <div className="containerBg">
+                    <div className="ml-24 mt-8">A L - Q U R ' A N</div>
+                    <div className="float-right search -mt-8 mr-2">
+                        <svg className="inline-block" width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.409 2C9.54811 2 7.72897 2.55183 6.18166 3.58571C4.63435 4.61959 3.42837 6.08908 2.71622 7.80836C2.00408 9.52763 1.81775 11.4195 2.1808 13.2447C2.54385 15.0698 3.43997 16.7464 4.75585 18.0622C6.07173 19.3781 7.74826 20.2742 9.57343 20.6373C11.3986 21.0003 13.2905 20.814 15.0097 20.1019C16.729 19.3897 18.1985 18.1837 19.2324 16.6364C20.2663 15.0891 20.8181 13.27 20.8181 11.409C20.8179 8.91366 19.8266 6.52052 18.0621 4.75602C16.2976 2.99152 13.9044 2.00016 11.409 2V2Z" stroke="#116A55" stroke-width="3" stroke-miterlimit="10"/>
+                            <path d="M18.4288 18.4288L25 25" stroke="#116A55" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round"/>
+                        </svg>
+                        <input type="text" className="w-2/4 ml-5 text-base" placeholder="Find Surah..."></input>
+                    </div>
+
+                    <div className="mt-24 ml-16">
+                        <div className="text-5xl font-extrabold title">Let’s Read Al-Qur’an</div>
+                        <div className="ml-2 mw-2/4">
+                            <div>QS : Al-Ahzab : 34</div>
+                            <div className="w-2/4 main-banner">
+                                Dan ingatlah apa yang dibacakan di rumahmu dari ayat-ayat Allah dan hikmah (sunnah nabimu). Sesungguhnya Allah adalah Maha Lembut lagi Maha Mengetahui. 
+                                <button className="float-right mt-5 mr-10 button-landing">GET STARTED</button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>   
+            </>
+        )
+    }
+}
+
+export default landingPage
