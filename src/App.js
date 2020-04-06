9359123ab107e6f4e05e1604333c93516c79bfbd
aznaqCre18 <azizaqibs@gmail.com>
2020-04-06 18:35:19 +0700

    page2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 import Landing from './container/landingPage/landingPage'
+import Open from './container/Open/open'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Landing />
+        <Router>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/open" component={Open} />
+        </Router>
       </div>
     )
   }
